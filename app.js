e542024eef8d2fc32203859a1365aad0aa45768c
ddmodifi <denise@wemodifi.com>
2019-09-17 17:13:15 -0700

    add new route

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/denise', (req, res) => res.send('Hello Denise!'))
 app.get('/teams', (req, res) => res.send('GET teams!'))
 app.get('/kendrick', (req, res) => res.send('GET kendrick!'))
+app.get('/newroute', (req, res) => res.send('JUST ADDED THIS NEW ROUTE!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
