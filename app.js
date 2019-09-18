9c4639610934d8cc3da9f3a3a68b6d722198b701
ddmodifi <denise@wemodifi.com>
2019-09-18 13:39:27 -0700

    print out some environment variables in the route

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
+
+const argv = require('yargs').argv
+const YAML = require('yaml')
+const fs = require('fs')
+
 const app = express()
 const port = 3000
 
+// const environment = argv.env || 'dev'
+// const file = fs.readFileSync(__dirname + '/secrets.yml', 'utf8') || ''
+// const secrets = YAML.parse(file)
+
+// if (secrets) {
+//     let env = environment === 'mig' ? 'sdx' : environment
+//     Object.keys(secrets[env]).forEach(key => {
+//         if (key !== '<<') {
+//             process.env[key] = secrets[env][key]
+//         }
+//     })
+// }
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/denise', (req, res) => res.send('Hello Denise!'))
+app.get('/denise', (req, res) => res.send('DEV DV: ' + process.env.DB_DATABASE_DEV + ', PROD DB: ' +process.env.DB_DATABASE_PROD + ', Hello Denise!'))
 app.get('/teams', (req, res) => res.send('GET teams!'))
 app.get('/kendrick', (req, res) => res.send('GET kendrick!'))
 app.get('/newroute', (req, res) => res.send('JUST ADDED THIS NEW ROUTE!'))
